8e6cd95beafd9b8e455b606a14084e93978a0556
gabr1el1 <gabrielhgxd@gmail.com>
2023-05-13 15:18:45 -0600

    Page tabs done

--- src/about.js
+++ src/about.js
@@ -1,4 +1,11 @@
+import fbIcon from './images/facebook.svg';
+import twIcon from './images/twitter.svg';
+import ytIcon from './images/youtube.svg';
+import tkIcon from './images/tiktok.svg';
 function aboutComponent(){
+    
+
+
     const main = document.querySelector("main");
     main.innerHTML = "";
 
@@ -8,19 +15,56 @@ function aboutComponent(){
     let mapa = document.createElement("div");
     mapa.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14952.751013518327!2d-103.43752041284174!3d20.45748030000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842f51975154be2b%3A0xa4b5aff12a03c2b6!2sTacos%20Arandas!5e0!3m2!1ses-419!2smx!4v1683561986971!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
     mapa = mapa.firstElementChild;
-    const info = document.createElement("div");
-    info.innerText = `
+    const infoSection = document.createElement("div");
+    const copy = document.createElement("div");
+    const titleCopy = document.createElement("h1");
+    titleCopy.innerText = "Who are we?";
+    const paragCopy = document.createElement("p");
+    paragCopy.innerText = `
 
-    Welcome to our tacos restaurant, where we take pride in serving the best Mexican cuisine in town! Our goal is to provide a memorable dining experience that will leave you feeling satisfied and wanting to come back for more.
-    
-    At our restaurant, we believe that food is not just fuel, but an experience that can be enjoyed with friends and family. That's why we use only the freshest ingredients and authentic recipes to create our delicious tacos, burritos, and other Mexican specialties.
+    Welcome to our tacos restaurant, 
     
     Our tacos are a true work of art, made with your choice of juicy meat or fresh vegetables, and topped with a variety of flavorful and colorful toppings. From the classic carne asada to the spicy and tangy al pastor, our tacos are sure to satisfy your cravings and leave you wanting more.
     
-     Come visit us today and experience the joy of authentic Mexican cuisine. Our friendly staff is ready to welcome you with open arms and show you what makes our restaurant so wonderful. We can't wait to serve you!`;
-    info.classList.add("info");
+    Come visit us today and experience the joy of authentic Mexican cuisine. Our friendly staff is ready to welcome you with open arms and show you what makes our restaurant so wonderful. We can't wait to serve you!`;
+    const contact = document.createElement("div");
+    const contactTitle = document.createElement("h1");
+    contactTitle.innerText = "Contac Us";
+    const socials = document.createElement("ul");
+    socials.classList.add("socials");
+    const facebook = document.createElement("li");
+    const facebookIcon = new Image();
+    facebookIcon.src = fbIcon;
+    facebook.appendChild(facebookIcon);
+    socials.appendChild(facebook);
+
+    const twitter = document.createElement("li");
+    const twitterIcon = new Image();
+    twitterIcon.src = twIcon;
+    twitter.appendChild(twitterIcon);
+    socials.appendChild(twitter);
+
+    const youtube = document.createElement("li");
+    const youtubeIcon = new Image();
+    youtubeIcon.src = ytIcon;
+    youtube.appendChild(youtubeIcon);
+    socials.appendChild(youtube);
+
+    const tiktok = document.createElement("li");
+    const tiktokIcon = new Image();
+    tiktokIcon.src = tkIcon;
+    tiktok.appendChild(tiktokIcon);
+    socials.appendChild(tiktok);
+
+    copy.appendChild(titleCopy);
+    copy.appendChild(paragCopy);
+    contact.appendChild(contactTitle);
+    contact.appendChild(socials);
+    infoSection.appendChild(copy);
+    infoSection.appendChild(contact);
+    infoSection.classList.add("info");
     aboutComponent.appendChild(mapa);
-    aboutComponent.appendChild(info);
+    aboutComponent.appendChild(infoSection);
     main.appendChild(aboutComponent);
 }
 
--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,80 @@
+import tacosBistec from './images/menu/tacos-bistec.jpg';
+import tacosChorizo from './images/menu/tacos-chorizo.jpg';
+import tacosLengua from './images/menu/tacos-lengua.jpg';
+import tacosPastor from './images/menu/tacos-pastor.jpg';
+import tacosSuadero from './images/menu/tacos-suadero.jpg';
+import horchata from './images/menu/horchata.jpg';
+import jamaica from './images/menu/jamaica.jpg';
+import arrozConLeche from './images/menu/arroz-con-leche.jpg';
+import jericalla from './images/menu/jericalla.jpg';
+
 function menuComponent(){
     const main = document.querySelector("main");
+    main.innerHTML = "";
+
     const menuComponent = document.createElement("div");
     menuComponent.classList.add("menu");
-    menuComponent.innerText = "Menu!";
+    
+    const tacosList = [
+    {"name":"Tacos de Bistec","price":1.0,"imageURL":tacosBistec},
+    {"name":"Tacos de Chorizo","price":1.0,"imageURL":tacosChorizo},
+    {"name":"Tacos de Lengua","price":1.5,"imageURL":tacosLengua},
+    {"name":"Tacos de Pastor","price":1.5,"imageURL":tacosPastor},
+    {"name":"Tacos de Suadero","price":0.9,"imageURL":tacosSuadero}
+    ];
+    
+    const drinksList = [
+    {"name":"Agua de horchata","price":2.0,"imageURL":horchata},
+    {"name":"Agua de jamaica","price":1.8,"imageURL":jamaica}
+    ];
 
-    main.innerHTML = menuComponent.innerHTML;
+    const dessertsList = [
+    {"name":"Arroz con leche","price":1.0,"imageURL":arrozConLeche},
+    {"name":"Jericalla","price":0.7,"imageURL":jericalla}
+    ]
+
+    
+    const tacos = document.createElement("div");
+    const drinks = document.createElement("div");
+    const desserts = document.createElement("div");
+    
+    const tacosTitle = document.createElement("h1");
+    tacosTitle.innerText = "Tacos";
+    const drinksTitle = document.createElement("h1");
+    drinksTitle.innerText = "Drinks";
+    const dessertsTitle = document.createElement("h1");
+    dessertsTitle.innerText = "Desserts"
+
+    tacos.appendChild(tacosTitle);
+    drinks.appendChild(drinksTitle);
+    desserts.appendChild(dessertsTitle);
+
+    loadToCategory(tacos, tacosList);
+    loadToCategory(drinks,drinksList);
+    loadToCategory(desserts, dessertsList);
+
+    menuComponent.appendChild(tacos);
+    menuComponent.appendChild(drinks);
+    menuComponent.appendChild(desserts);
+
+
+    main.appendChild(menuComponent);
 }
+
+function loadToCategory(category, listItems){
+    listItems.forEach(element => {
+        const item = document.createElement("div");
+        const html = 
+        `<h2>${element.name}</h2>
+        <img src=${element.imageURL}>
+        <p>$${element.price}</p>`;
+        item.innerHTML = html;
+        item.classList.add("item-menu");
+        category.appendChild(item);
+    });
+}
+
+
+
 
 export {menuComponent}
